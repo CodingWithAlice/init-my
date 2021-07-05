20668f5a7081c862437cff889ef8c2d917057f73
CodingWithAlice <2269131254@qq.com>
2021-07-05 16:51:30 +0800

    modify：node 事件循环案例2

--- src/assets/nodeEventLoop.js
+++ src/assets/nodeEventLoop.js
@@ -1,14 +1,22 @@
-setTimeout(() => {
-    console.log("time1");
-    process.nextTick(()=>{
-        console.log("nextTick2");
-    });
-}, 0);
-console.log("start")
-process.nextTick(()=>{
-    console.log("nextTick1");
-    setTimeout(()=>{
-        console.log("time2");
-    });
-});
+// setTimeout(() => {
+//     console.log("time1");
+//     process.nextTick(()=>{
+//         console.log("nextTick2");
+//     });
+// }, 0);
+// console.log("start")
+// process.nextTick(()=>{
+//     console.log("nextTick1");
+//     setTimeout(()=>{
+//         console.log("time2");
+//     });
+// });
 // 上述代码输出结果：start  nextTick1 time1 nextTick2 time2
+setTimeout(() => {
+    console.log('timeout');
+}, 0);
+
+setImmediate(() => {
+    console.log('immediate');
+});
+// 答案是不一定
