b68777da725b2ca486cf417ed1cf13d9444b595e
CodingWithAlice <2269131254@qq.com>
2021-07-05 16:52:24 +0800

    modify：node事件循环 案例3

--- src/assets/nodeEventLoop.js
+++ src/assets/nodeEventLoop.js
@@ -12,11 +12,21 @@
 //     });
 // });
 // 上述代码输出结果：start  nextTick1 time1 nextTick2 time2
-setTimeout(() => {
-    console.log('timeout');
-}, 0);
+// setTimeout(() => {
+//     console.log('timeout');
+// }, 0);
 
-setImmediate(() => {
-    console.log('immediate');
-});
+// setImmediate(() => {
+//     console.log('immediate');
+// });
 // 答案是不一定
+const fs = require('fs');
+fs.readFile(__filename, () => {
+    setTimeout(() => {
+        console.log('timeout');
+    }, 0);
+    setImmediate(() => {
+        console.log('immediate');
+    });
+});
+// 答案永远是： immediate timeout
