bcd79bb32acc4ddf5351df40fae494b9f68ea1b3
CodingWithAlice <2269131254@qq.com>
2022-08-07 11:19:26 +0800

    reload：以工厂函数取代构造函数

--- src/重构/createStatementData.js
+++ src/重构/createStatementData.js
@@ -37,6 +37,10 @@ class PerformanceCalculator {
   }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+  new PerformanceCalculator(aPerformance, aPlay);
+}
+
 export default function createStatementData(invoice, plays) {
   const result = {};
   result.customer = invoice.customer;
@@ -46,7 +50,7 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play =  calculator.play;
     result.amount = calculator.amount;
