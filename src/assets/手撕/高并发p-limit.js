5f3e69c8c4718333a0904f80364e75918cecaf9d
CodingWithAlice <2269131254@qq.com>
2025-03-15 10:25:16 +0800

    刷题：p-limit + 二叉树

--- src/assets/手撕/高并发p-limit.js
+++ src/assets/手撕/高并发p-limit.js
@@ -1,24 +1,64 @@
-import pLimit from 'p-limit';
-
-// 创建一个限制器实例，设置并发限制为 2
+import pLimit, { limitFunction } from 'p-limit';
 const limit = pLimit(2);
+const arr = Array.from({ length: 20 }, (it, index) => {
+    return limit(() => new Promise(resolve => setTimeout(resolve, index * 100)))
+})
 
-// 模拟异步任务
-const asyncTask = (id) => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            console.log(`Task ${id} completed`);
-            resolve(id);
-        }, 1000);
-    });
-};
+const arr2 = Array.from({ length: 20 }, (it, index) => {
+    return limitFunction(() => new Promise(resolve => setTimeout(resolve, index * 100)), { concurrency: 2 })
+})
 
-// 创建任务数组
-const tasks = Array.from({ length: 5 }, (_, index) =>
-    limit(() => asyncTask(index + 1))
-);
+Promise.all(arr).then(res => res);
+Promise.all(arr2).then(res => res);
 
-// 执行所有任务
-Promise.all(tasks).then((results) => {
-    console.log('All tasks completed:', results);
-});
+// 源码实现
+function validate(limit) { if (!Number.isInteger(limit) || limit < 0) { throw new Error('limit') } }
+function pLimit1(concurrency) {
+    validate(concurrency);
+    const queue = [];
+    let current = 0; // 1、可变变量，使用 let
+    const next = () => {
+        current--;
+        if (queue.length) {
+            queue.shift()();
+        }
+    }
+    const run = async (resolve, fn, arg) => { // 2、run 执行函数的时候，要注意 fn 是异步函数
+        current++;
+        try {
+            const res = await fn(...arg);
+            resolve(res);
+        } catch (e) {
+            throw new Error('e') // 3、执行遇到错误时，抛出一个拒绝的 promise - resolve(Promise.reject(e))
+        }
+        next(); // 4、执行下一个任务
+    }
+    const generate = (cb, ...args) => {
+        return new Promise((resolve) => {
+            if (current < concurrency) {
+                run(resolve, cb, args)
+            } else {
+                queue.push(run.bind(null, resolve, cb, args))
+            }
+        })
+    }
+    return generate;
+}
+// 使用Promise.race 来实现并发控制
+async function execute(arr, concurrency = 0) {
+    const indexedArr = arr.map((it, index) => {
+        return { index, promise: it.then(value => ({ value, key: index })) }
+    })
+    const activeTasks = indexedArr.splice(0, concurrency);
+    const result = [];
+    while(activeTasks.length) {
+        const {key ,value} = await Promise.race(activeTasks.map(it => it.promise));
+        const currentIndex = activeTasks.findIndex(it => it.index === key);
+        activeTasks.splice(currentIndex, 1);
+        result[key] = value;
+        if(indexedArr.length > 0) {
+            activeTasks.push(indexedArr.shift())
+        }
+    }
+    return result
+}
