cb30a90c334ab5720535f22df9e1445b669aa512
CodingWithAlice <2269131254@qq.com>
2025-03-21 15:00:52 +0800

    刷题：p-limit*2 + KOA2源码 + nextTick + promise + 二叉树 + 中间件

--- src/assets/手撕/高并发p-limit.js
+++ src/assets/手撕/高并发p-limit.js
@@ -1,66 +1,60 @@
-import pLimit, { limitFunction } from 'p-limit';
+import pLimit, { limitFunction } from "p-limit";
 const limit = pLimit(5);
-const arr = Array.from({ length: 20 }, (it, index) => {
-    return limit(() => new Promise(resolve => setTimeout(resolve, index * 100)))
+const arr = Array.from({ length: 30 }, (it, index) => {
+    return limit(() => new Promise(resolve => setTimeout(resolve, index * 99)))
 })
 Promise.all(arr).then(console.log)
-const arrMixed = Array.from({ length: 20 }, (it, index) => {
-    return limitFunction(() => new Promise(resolve => setTimeout(resolve, index * 80)), { concurrency: 5 })
+const arr2 = Array.from({ length: 30 }, (it, index) => {
+    return limitFunction(() => new Promise(resolve => setTimeout(resolve, index * 99)))
 })
-Promise.all(arrMixed).then(console.log)
-// 源码
+Promise.all(arr2).then(console.log)
 function validate(limit) {
-    if (!Number.isInteger(limit) || limit < 0) {
-        throw new Error('err concurrency')
-    }
+    if (!Number.isInteger(limit) || limit < 0) { throw new Error('limit err') }
 }
 function pLimit(concurrency) {
     validate(concurrency);
     let activeCount = 0;
     const activeQueue = [];
     const next = () => {
-        activeCount--;
+        activeCount++;  // ❌ 1、next 执行时，代表一个任务执行完成，activeCount--
         if (activeQueue.length) {
             activeQueue.shift()();
         }
     }
     const run = async (fn, resolve, arg) => {
-        activeCount++;
+        activeCount--; // ❌  2、 run 执行时代表一个任务开始执行 activeCount++
+        // ❌ 3、少了 try-catch
         try {
             const res = await fn(...arg);
-            resolve(res)
-        } catch (e) {
-            return Promise.reject(e)
-        }
-        next()
+            resolve(res);
+        } catch (e) { return Promise.reject(e) }
+        next();
     }
     const generator = (cb, ...arg) => {
-        // ❌ 1、返回 promise：return new Promise((resolve) => {})
-        if (activeCount < concurrency) {
-            run(cb, resolve, arg)
-        } else {
-            activeQueue.push(run.bind(null, cb, resolve, arg))
-        }
+        return new Promise((resolve) => {
+            if (activeCount < concurrency) {
+                run(cb, resolve, arg)
+            } else {
+                activeQueue.push(run.bind(null, cb, resolve, arg))
+            }
+        })
     }
-    return generator
+    return generator;
 }
-
 async function execute(arr, concurrency) {
-    const indexedArr = arr.map((it, index) => {
-        return {
-            index,
-            promise: Promise.resolve(it).then(res => ({ key: index, value: res }))
-        }
-    })
-    const activeArr = indexedArr.splice(0, concurrency);
+    const indexedArr = arr.map((it, index) => ({
+        index,
+        promise: () => Promise.resolve(it).then(value => ({ key: index, value }))
+    }))
     const result = [];
-    while (activeArr.length) {
-        const { key, value } = await Promise.race(activeArr.map(it => it.promise));
-        const index = activeArr.findIndex(it => it.index === key);
-        activeArr.splice(index, 1);
+    const queue = indexedArr.splice(0, concurrency);
+    while (queue.length) {
+        const { key, value } = await Promise.race(queue.map(it => it.promise));
+        const index = queue.findIndex(it => it.index === key);
         result[key] = value;
+        queue.splice(index, 1);
         if (indexedArr.length) {
-            activeArr.push(indexedArr.shift())
+            queue.push(indexedArr.shift())
         }
     }
     return result
